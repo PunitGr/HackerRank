7c882f6baf390dcf68590d2d2a73406980b45efc
Punit Grover <groove679@gmail.com>
2016-07-25 13:52:21 +0530

    Add nested-list question solutoion

---
+++ nested-list.py
@@ -0,0 +1,21 @@
+# find the second max scorer in the nested list
+
+N = int(input())
+
+myList = list()
+for i in range(0, N):
+    myList.append([input(), float(input())])
+
+scores = set(myList[x][1] for x in range(0, N))
+scores = list(scores)
+scores.sort()
+
+students = list()
+for i in range(0, N):
+    if scores[1] == myList[i][1]:
+        students.append(myList[i][0])
+        students = list(students)
+
+students.sort()
+for x in students:
+    print (x)
