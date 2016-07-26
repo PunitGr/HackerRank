594ebb1dcc0866d03d31522231df3c853143f7c6
Punit Grover <groove679@gmail.com>
2016-07-26 23:57:19 +0530

    add swap case problem solution

---
+++ swapcase.py
@@ -0,0 +1,8 @@
+# change case of string
+S = list(input())
+for i in range(len(S)):
+    if S[i].isupper() == True:
+        S[i] = S[i].lower()
+    else:
+        S[i] = S[i].upper()
+print (''.join(S))
