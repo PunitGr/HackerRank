ad01c34b849f54cd72b302f8c00e6a2ec8e32a39
Punit Grover <groove679@gmail.com>
2016-08-01 20:57:35 +0530

    add minion game problem solution

---
+++ Strings/minion-game.py
@@ -0,0 +1,17 @@
+S = input()
+n = len(S)
+stuart = 0
+kevin = 0
+for i in range(n):
+    if S[i] in ['A', 'E', 'I', 'O', 'U']:
+        kevin += n - i
+    else:
+        stuart += n - i
+
+if stuart > kevin:
+    print "Stuart", stuart
+elif stuart < kevin:
+    print "Kevin", kevin
+else:
+    print "Draw"
+
