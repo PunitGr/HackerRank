8c5ef48d15f0b6029fd3c9c92dae908a1f1a523d
Punit Grover <groove679@gmail.com>
2016-08-22 20:33:32 +0530

    add collection counter problem solution

---
+++ Python/Collections/collectionsCounter.py
@@ -0,0 +1,13 @@
+#!/bin/python
+from collections import Counter
+x = int(input())
+sizes = Counter(map(int, input().split()))
+n = int(input())
+money = 0
+for i in range(n):
+    shoe_size, price = map(int, input().split())
+    if sizes[shoe_size] > 0:
+        sizes[shoe_size] = sizes[shoe_size] - 1
+        money = money + price
+print(money)
+
