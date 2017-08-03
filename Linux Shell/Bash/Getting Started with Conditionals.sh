ed6e1eb3ac78bac1063db5b7cadec3f7f5a199f9
PunitGr <groove679@gmail.com>
2017-08-03 23:23:52 +0530

    Add linux shell solutions

---
+++ Linux Shell/Bash/Getting Started with Conditionals.sh
@@ -0,0 +1,10 @@
+read char
+if [ $char == 'Y' ]; then
+    echo "YES"
+elif [ $char == 'N' ]; then
+    echo "NO"
+elif [ $char == 'y' ]; then
+    echo "YES"
+elif [ $char == 'n' ]; then
+    echo "NO"
+fi
